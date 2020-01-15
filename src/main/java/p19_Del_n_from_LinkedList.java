335e1ee3b51ed70a3313afed7fc832294a985a93
Feng-Zhao <zf6091994@gmail.com>
2020-01-15 13:31:22 +1100

    p19

---
+++ src/main/java/p19_Del_n_from_LinkedList.java
@@ -0,0 +1,33 @@
+
+//Definition for singly-linked list.
+
+
+
+public class p19_Del_n_from_LinkedList {
+    public ListNode sol_1(ListNode head, int n) {
+        ListNode first = head;
+        ListNode target = head;
+        int count = 0;
+        while(first != null){
+            first = first.next;
+            if(count > n){
+                target = target.next;
+            }
+            count++;
+        }
+        ListNode temp = target.next;
+        if(count > n){
+            target.next = temp.next;
+            return head;
+        }
+        else{
+            return head.next;
+        }
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+}
