f5fa16e5b93c7a0c75392bc93131884be1b5a0fd
Feng-Zhao <zf6091994@gmail.com>
2020-01-15 13:54:33 +1100

    p20 better sol

--- src/main/java/p20_Parentheses_Validation.java
+++ src/main/java/p20_Parentheses_Validation.java
@@ -8,36 +8,35 @@ public class p20_Parentheses_Validation {
             add('[');
             add('{');
         }};
-        Map<Character,Character> pair = new HashMap<Character, Character>() {{
-            put(')','(');
-            put(']','[');
-            put('}','{');
+        Map<Character, Character> pair = new HashMap<Character, Character>() {{
+            put(')', '(');
+            put(']', '[');
+            put('}', '{');
         }};
         Stack<Character> stack = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
             if (lefts.contains(s.charAt(i))) {
                 stack.push(s.charAt(i));
-            }
-            else{
-                if(stack.empty()){
+            } else {
+                if (stack.empty()) {
                     return false;
                 }
-                if(stack.peek() == pair.get(s.charAt(i))){
+                if (stack.peek() == pair.get(s.charAt(i))) {
                     stack.pop();
-                }
-                else{
+                } else {
                     return false;
                 }
             }
         }
-        if(stack.empty()){
+        if (stack.empty()) {
             return true;
         }
         return false;
     }
+
     //a shorter format
     //comes from https://leetcode.com/problems/valid-parentheses/discuss/9178/Short-java-solution
-    public boolean isValid(String s) {
+    public boolean sol_2(String s) {
         Stack<Character> stack = new Stack<Character>();
         for (char c : s.toCharArray()) {
             if (c == '(')
@@ -50,5 +49,28 @@ public class p20_Parentheses_Validation {
                 return false;
         }
         return stack.isEmpty();
+    }
+    //a faster method by manually implement a stack using char[];
+    public boolean sol_3(String s) {
+        char[] stack = new char[s.length()];
+        int head = 0;
+
+        for (char c : s.toCharArray()) {
+            switch (c) {
+                case '(':
+                    stack[head++] = ')';
+                    break;
+                case '{':
+                    stack[head++] = '}';
+                    break;
+                case '[':
+                    stack[head++] = ']';
+                    break;
+                default:
+                    if (head == 0 || stack[--head] != c) return false;
+                    break;
+            }
+        }
+        return head == 0;
     }
 }
