daac2a9a3a86526e2f843302e0b184664722dc29
Feng-Zhao <zf6091994@gmail.com>
2020-01-17 23:42:45 +1100

    p24+

--- src/main/java/p24_Swap_Nodes_in_Pairs.java
+++ src/main/java/p24_Swap_Nodes_in_Pairs.java
@@ -19,6 +19,15 @@ public class p24_Swap_Nodes_in_Pairs {
         }
         return start.next;
     }
+    //recursive solution
+    public ListNode swapPairs(ListNode head) {
+        if ((head == null)||(head.next == null))
+            return head;
+        ListNode n = head.next;
+        head.next = swapPairs(head.next.next);
+        n.next = head;
+        return n;
+    }
 
     static public void main(String[] arg){
         p24_Swap_Nodes_in_Pairs sol = new p24_Swap_Nodes_in_Pairs();
