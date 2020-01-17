281e061aa2250c8f02bda9c2788e2a2fbbfa71e1
Feng-Zhao <zf6091994@gmail.com>
2020-01-17 23:40:30 +1100

    p24

--- src/main/java/p23_k_sorted_list_merge.java
+++ src/main/java/p23_k_sorted_list_merge.java
@@ -5,26 +5,26 @@ import java.util.Queue;
 
 public class p23_k_sorted_list_merge {
     public ListNode sol_1(ListNode[] lists) {
-        if(lists.length == 0){
+        if (lists.length == 0) {
             return null;
         }
-        if(lists.length == 1){
+        if (lists.length == 1) {
             return lists[0];
         }
         ListNode head = lists[0];
         for (int i = 1; i < lists.length; i++) {
-            head = mergeTwoLists(head,lists[i]);
+            head = mergeTwoLists(head, lists[i]);
         }
         return head;
     }
 
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2){
-        if(l1 == null) return l2;
-        if(l2 == null) return l1;
-        if(l1.val < l2.val){
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        if (l1.val < l2.val) {
             l1.next = mergeTwoLists(l1.next, l2);
             return l1;
-        } else{
+        } else {
             l2.next = mergeTwoLists(l1, l2.next);
             return l2;
         }
@@ -36,8 +36,9 @@ public class p23_k_sorted_list_merge {
     public ListNode sol_2(ListNode[] lists) {
 //        if (lists==null||lists.length ==0) return null;
 
-        Queue<ListNode> heap = new PriorityQueue(new Comparator<ListNode>(){
-            @Override public int compare(ListNode l1, ListNode l2) {
+        Queue<ListNode> heap = new PriorityQueue(new Comparator<ListNode>() {
+            @Override
+            public int compare(ListNode l1, ListNode l2) {
                 return l1.val - l2.val;
             }
         });
@@ -49,5 +50,66 @@ public class p23_k_sorted_list_merge {
             if (tail.next != null) heap.offer(tail.next);
         }
         return head.next;
+    }
+
+    //improved by controlling the length of each sub problem
+    //comes from https://leetcode.com/problems/merge-k-sorted-lists/discuss/10805/Beat-94.84-3ms-divide-conquer-Java-solution!
+    public ListNode sol_3(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+        return reduce(map(lists, 0, lists.length / 2), map(lists, lists.length / 2 + 1, lists.length - 1));
+    }
+
+    private ListNode map(ListNode[] lists, int from, int to) {
+        if (from > to) {
+            return null;
+        }
+        if (from == to) {
+            return lists[from];
+        }
+        if (from + 1 == to) {
+            return reduce(lists[from], lists[to]);
+        }
+        return reduce(map(lists, from, (from + to) / 2), map(lists, (from + to) / 2 + 1, to));
+    }
+
+    private ListNode reduce(ListNode first, ListNode second) {
+        if (first == null) {
+            return second;
+        }
+        if (second == null) {
+            return first;
+        }
+        ListNode head, pre;
+        if (first.val < second.val) {
+            head = first;
+            pre = first;
+            first = first.next;
+        } else {
+            head = second;
+            pre = second;
+            second = second.next;
+        }
+        while (true) {
+            if (first == null) {
+                pre.next = second;
+                break;
+            }
+            if (second == null) {
+                pre.next = first;
+                break;
+            }
+            if (first.val < second.val) {
+                pre.next = first;
+                pre = pre.next;
+                first = first.next;
+            } else {
+                pre.next = second;
+                pre = pre.next;
+                second = second.next;
+            }
+        }
+        return head;
     }
 }
---
+++ src/main/java/p24_Swap_Nodes_in_Pairs.java
@@ -0,0 +1,41 @@
+import java.awt.image.TileObserver;
+import java.util.List;
+
+public class p24_Swap_Nodes_in_Pairs {
+    public ListNode sol_1(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode left = head, right = head.next;
+        ListNode start = new ListNode(0);
+        ListNode temp = start;
+        while(left != null && right != null) {
+            temp.next = right;
+            left.next = right.next;
+            right.next = left;
+            temp = left;
+            left = left.next;
+            right = left!=null?left.next:null;
+        }
+        return start.next;
+    }
+
+    static public void main(String[] arg){
+        p24_Swap_Nodes_in_Pairs sol = new p24_Swap_Nodes_in_Pairs();
+        ListNode a = new ListNode(1);
+        ListNode start = a;
+        a.next = new ListNode(2);
+        a = a.next;
+        a.next = new ListNode(3);
+        a = a.next;
+        a.next = new ListNode(4);
+        a=a.next;
+        a.next = null;
+
+        ListNode res = sol.sol_1(start);
+        while(res != null){
+            System.out.println(res.val);
+            res = res.next;
+        }
+    }
+}
