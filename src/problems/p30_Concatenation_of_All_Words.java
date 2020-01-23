b542a02358d5e242de7f9d2f901245158cb3fc22
Feng Zhao <zf6091994@gmail.com>
2020-01-23 19:35:29 +1100

    30 unfinished

--- src/problems/p30_Concatenation_of_All_Words.java
+++ src/problems/p30_Concatenation_of_All_Words.java
@@ -2,15 +2,34 @@ package problems;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class p30_Concatenation_of_All_Words {
     public List<Integer> findSubstring(String s, String[] words) {
-        HashMap<Integer,String> map = new HashMap<>();
-        for (int i = 0; i < words.length; i++) {
-
-            map.put(i,words[i]);
+        if(words.length == 0){
+            return null;
         }
-        //TODO
+        int sizeOfWords = words.length;
+        int lenOfWord = words[0].length();
+        if(s.length() < sizeOfWords * lenOfWord){
+            return null;
+        }
+        Map<String,Integer> words2prime = new HashMap<>();
+        for (int i = 1; i <= words.length; i++) {
+            words2prime.put(words[i], thPrime(i));
+        }
+
+        for (int i = 0; i < s.length()-sizeOfWords*lenOfWord; i++) {
+            String firstSub = s.substring(i, i + lenOfWord);
+            if(words2prime.containsKey(firstSub)){
+                
+            }
+        }
         return null;
+    }
+
+    private int thPrime(int n){
+        //TODO
+        return n;
     }
 }
