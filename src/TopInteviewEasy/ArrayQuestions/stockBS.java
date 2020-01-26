fb01c4c035b9f09efce9bb636107e6d68f49a747
Feng-Zhao <zf6091994@gmail.com>
2020-01-26 22:37:21 +1100

    stcokBS

--- src/TopInteviewEasy/ArrayQuestions/stockBS.java
+++ src/TopInteviewEasy/ArrayQuestions/stockBS.java
@@ -1,5 +1,20 @@
 package TopInteviewEasy.ArrayQuestions;
 
+import java.util.Arrays;
+import java.util.Map;
+
 public class stockBS {
+    public int maxProfit(int[] prices) {
+        if(prices.length < 1){
+            return 0;
+        }
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if(prices[i] - prices[i-1] > 0){
+                profit += prices[i] - prices[i-1];
+            }
+        }
+        return profit;
+    }
 
 }
