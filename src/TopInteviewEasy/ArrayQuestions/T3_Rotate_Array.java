95052796ec6813af3cf2e11698c6666dc2241e05
Feng-Zhao <zf6091994@gmail.com>
2020-01-28 11:19:45 +1100

    t3_rotate

--- src/TopInteviewEasy/ArrayQuestions/T3_Rotate_Array.java
+++ src/TopInteviewEasy/ArrayQuestions/T3_Rotate_Array.java
@@ -4,19 +4,29 @@ import java.util.*;
 
 public class T3_Rotate_Array {
     public void rotate(int[] nums, int k) {
-        Queue<Integer> newList = new ArrayDeque<>();
+        if(nums.length < 2){
+            return;
+        }
+        else if (k >= nums.length){
+            rotate(nums,k-nums.length);
+            return;
+        }
+        Deque<Integer> deque = new ArrayDeque<>();
         for (int i = nums.length-1; i >= nums.length-k; i--) {
-            newList.add(nums[i]);
+            deque.addFirst(nums[i]);
         }
         for (int i = 0; i < nums.length-k; i++) {
-            newList.add(nums[i]);
+            deque.add(nums[i]);
         }
-//        nums = newList.toArray(new int[0]);
+
         for (int i = 0; i < nums.length; i++) {
-            System.out.println(nums[i]);
+            nums[i] = deque.poll();
         }
+        // for (int i = 0; i < nums.length; i++) {
+        //     System.out.println(nums[i]);
+        // }
     }
-    static public void main(String arg[]){
+    static public void main(String[] args){
         T3_Rotate_Array Sol = new T3_Rotate_Array();
         int[] nums = {1,2,3,4,5,6,7};
         Sol.rotate(nums,3);
